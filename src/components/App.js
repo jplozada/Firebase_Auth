71686cb8f387681769da262804a768fc0979448a
jplozada <juxnpx@gmail.com>
2021-02-15 03:30:33 -0400

    React-router-dom installed and added to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import Signup from "./Signup";
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from "../context/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <AuthProvider>
       <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
         <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup/>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/signup" component={Signup} />
+              </Switch>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
-    </AuthProvider>
   );
 }
 
