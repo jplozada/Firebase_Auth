4862f71bd94a564e9844a617b39a02b0b523ddf9
jplozada <juxnpx@gmail.com>
2021-02-16 21:54:57 -0400

    Reset password functionality completed

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Login from './Login';
 import PrivateRoute from './PrivateRoute';
+import ForgotPassword from './ForgotPassword';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
                 <PrivateRoute exact path="/" component={Dashboard} />
                 <Route path="/signup" component={Signup} />
                 <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
               </Switch>
             </AuthProvider>
           </Router>
